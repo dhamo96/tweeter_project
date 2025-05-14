909a3388ad15c1b570a47e6e563f1d3adcc66e3c
Dharmendra Patel <dharmendrapatel1911996@gmail.com>
2025-05-14 08:37:11 +0530

    Update settings.py

--- tweeter_project/settings.py
+++ tweeter_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-o-*9xk_6#dd+)vq$$+jsq-l@gz&dco-_1^qg3*ololhu-q5*#&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
